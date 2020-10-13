4925017022aa20b7c2c33d50e6e86eb463ee621f
fsdevon <fullstackdevovernight@gmail.com>
2020-10-14 00:03:21 +0700

    Updated place app.

--- place_app/lib/providers/great_places.dart
+++ place_app/lib/providers/great_places.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:place_app/helpers/db_helper.dart';
+import 'package:place_app/helpers/location_helper.dart';
 import 'package:place_app/models/place.dart';
 
 class GreatPlaces with ChangeNotifier {
@@ -10,14 +12,52 @@ class GreatPlaces with ChangeNotifier {
     return [..._items];
   }
 
-  void addPlace(String title, File pickedImage) {
+  Place findById(String id) {
+    return _items.firstWhere((place) => place.id == id);
+  }
+
+  Future<void> addPlace(
+      String title, File pickedImage, PlaceLocation pickedLocation) async {
+    final address = await LocationHelper.getPlaceAddress(
+        pickedLocation.latitude, pickedLocation.longitude);
+
+    final updateLocation = PlaceLocation(
+        latitude: pickedLocation.latitude,
+        longitude: pickedLocation.longitude,
+        address: address);
+
     final newPlace = Place(
         id: DateTime.now().toString(),
         title: title,
-        location: null,
+        location: updateLocation,
         image: pickedImage);
 
     _items.add(newPlace);
+    notifyListeners();
+
+    DBHelper.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+      'loc_lat': newPlace.location.latitude,
+      'loc_lng': newPlace.location.longitude,
+      'address': newPlace.location.address
+    });
+  }
+
+  Future<void> fetchAndSetPlaces() async {
+    final dataList = await DBHelper.getData('user_places');
+    _items = dataList
+        .map((item) => Place(
+            id: item['id'],
+            title: item['title'],
+            location: PlaceLocation(
+                latitude: item['loc_lat'],
+                longitude: item['loc_lng'],
+                address: item['address']),
+            image: File(item['image'])))
+        .toList();
+
     notifyListeners();
   }
 }
--- place_app/lib/screens/places_list_screen.dart
+++ place_app/lib/screens/places_list_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:place_app/providers/great_places.dart';
 import 'package:place_app/screens/add_place_screen.dart';
+import 'package:place_app/screens/place_detail_screen.dart';
 import 'package:provider/provider.dart';
 
 class PlacesListScreen extends StatelessWidget {
@@ -17,24 +18,39 @@ class PlacesListScreen extends StatelessWidget {
               })
         ],
       ),
-      body: Consumer<GreatPlaces>(
-        builder: (context, greatPlaces, ch) => greatPlaces.items.length <= 0
-            ? ch
-            : ListView.builder(
-                itemCount: greatPlaces.items.length,
-                itemBuilder: (context, index) => ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: FileImage(greatPlaces.items[index].image),
-                  ),
-                  title: Text(greatPlaces.items[index].title),
-                  onTap: () {
-                    // Go to the home page
-                  },
+      body: FutureBuilder(
+        future: Provider.of<GreatPlaces>(context, listen: false)
+            .fetchAndSetPlaces(),
+        builder: (ctx, snapshot) => snapshot.connectionState ==
+                ConnectionState.waiting
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Consumer<GreatPlaces>(
+                builder: (context, greatPlaces, ch) =>
+                    greatPlaces.items.length <= 0
+                        ? ch
+                        : ListView.builder(
+                            itemCount: greatPlaces.items.length,
+                            itemBuilder: (context, index) => ListTile(
+                              leading: CircleAvatar(
+                                backgroundImage:
+                                    FileImage(greatPlaces.items[index].image),
+                              ),
+                              title: Text(greatPlaces.items[index].title),
+                              subtitle: Text(
+                                  greatPlaces.items[index].location.address),
+                              onTap: () {
+                                Navigator.of(context).pushNamed(
+                                    PlaceDetailScreen.routeName,
+                                    arguments: greatPlaces.items[index].id);
+                              },
+                            ),
+                          ),
+                child: Center(
+                  child: const Text('Got no places et, start adding some!'),
                 ),
               ),
-        child: Center(
-          child: const Text('Got no places et, start adding some!'),
-        ),
       ),
     );
   }
